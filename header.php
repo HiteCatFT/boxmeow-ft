beb79a39afb174ff4ad6f3b43352cd379d375cfe
HiteCatFT <lokchonweng@gmail.com>
2023-07-10 18:27:56 +0800

    Header style update

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link id="theme-style" href="/wordpress/wp-content/themes/boxmeow-ft/assets/dist/main.css" rel="stylesheet" />
 	<link id="theme-style" href="/wordpress/wp-content/themes/boxmeow-ft/assets/dist/style.css" rel="stylesheet" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -13,42 +14,43 @@
 	<script src="/wordpress/wp-content/themes/boxmeow-ft/assets/js/plugin.js"></script>
 </head>
 
-<body style="background-color:#0C3094;">
+<body>
 	<header class="header-global">
-		<nav id="navbar-main" class="navbar navbar-expand-lg navbar-light navbar-transparent headroom">
-			<div class="container">
-				<a class="logo navbar-brand font-weight-bold" href="<?php echo home_url(); ?>" title="boxmoe">
+		<nav class="navbar navbar-expand-lg navbar-dark navbar-backgroud-ft">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
-				</a>
-				<button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon mt-2">
-						<span class="navbar-toggler-bar bar1"></span>
-						<span class="navbar-toggler-bar bar2"></span>
-						<span class="navbar-toggler-bar bar3"></span>
-					</span>
 				</button>
-				<div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
-					<ul class="navbar-nav navbar-nav-hover mx-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Home</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">About</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Services</a>
+							<a class="nav-link" href="#">Link</a>
 						</li>
 						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Dropdown Menu
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Dropdown
 							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">Item 1</a>
-								<a class="dropdown-item" href="#">Item 2</a>
-								<a class="dropdown-item" href="#">Item 3</a>
-							</div>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li>
+									<hr class="dropdown-divider">
+								</li>
+								<li><a class="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled">Disabled</a>
 						</li>
 					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success" type="submit">Search</button>
+					</form>
 				</div>
 			</div>
 		</nav>
